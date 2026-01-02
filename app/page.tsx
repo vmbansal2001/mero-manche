924ed211f960e47eee9604b592f17a4bba7262b7
vmbansal2001 <vmbansal2001@gmail.com>
2026-01-03 02:34:33 +0530

    Update app icons with new versions for various sizes
    - Replaced existing PNG icons with updated versions for sizes 72x72, 96x96, 128x128, 144x144, 152x152, 192x192, 384x384, and 512x512.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Mero Manche</div>;
+  return (
+    <div className="text-2xl font-bold text-center flex h-screen items-center justify-center">
+      Mero Manche
+    </div>
+  );
 }
